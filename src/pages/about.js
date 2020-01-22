45a6b0ba55c8b10b739b4281def37b54855b7a73
lewi-g <dr.lewi@gmail.com>
2020-01-22 17:03:46 -0500

    Updates About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import Layout from '../components/layout'
+
 import Head from '../components/head'
+
 
 function AboutPage(){
   return (
     <Layout>
       <Head title="About"/>
       <h1>About Me</h1>
-      <p>I am a Javascript Bootcamp grad looking for opportunities to get better as a software developer.</p>
-      <p> <Link to="/contact">Want to work with me? Reach out.</Link></p>
+      <p>I have written end-to-end tests for an in-house sales tracking app using Cypress.</p>
+      <p>I have ran summer camps teaching HTML, CSS and JS to children ages 7 to 14.</p>
+      <p>I’m excited about exploring opportunities to grow as a software developer.</p>
+      <p> <Link to="/contact">Let's chat.</Link></p>
     </Layout>
   )
 }
