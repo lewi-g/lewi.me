1098dadbe3e2303d56cedae7a096fae1a83d3757
lewi-g <dr.lewi@gmail.com>
2020-03-06 18:26:56 -0500

    Updates footer to add github, linkedin and email

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {graphql, useStaticQuery} from 'gatsby'
+import {graphql, useStaticQuery, Link, withPrefix} from 'gatsby'
+import footerStyles from './footer.module.scss'
+
 
 function Footer() {
   const data = useStaticQuery(graphql`
@@ -9,12 +11,43 @@ function Footer() {
           author
         }
       }
+
     }
   `)
   
   return (
     <footer>
-      <p>Created by {data.site.siteMetadata.author}, ©2020</p>
+      <nav className={footerStyles.navList}>   
+        <li>
+          <a target="_blank" rel="noopener noreferrer" 
+             className={footerStyles.navItem} 
+             activeClassName={footerStyles.activeNavItem}
+              href="https://www.github.com/lewi-g"
+            >
+            GitHub
+          </a> 
+        </li>
+        <li >
+          <a 
+            target="_blank" rel="noopener noreferrer" 
+            href="https://www.linkedin.com/in/lewi-g/"
+            className={footerStyles.navItem} 
+            activeClassName={footerStyles.activeNavItem}
+            >
+            LinkedIn
+          </a> 
+        </li>
+        <li >
+          <a 
+            href="mailto:lewi.dev@gmail.com"
+            className={footerStyles.navItem} 
+            activeClassName={footerStyles.activeNavItem}
+          >
+            Email
+          </a>
+        </li>
+      </nav>
+      <p className={footerStyles.copyright}>Created by {data.site.siteMetadata.author}, ©2020</p>
     </footer>
   )
 }
