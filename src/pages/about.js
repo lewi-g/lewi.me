e832da58ce90af860d95ffcd8d0464dd788097a6
lewi-g <dr.lewi@gmail.com>
2020-03-06 18:28:12 -0500

    Adds image to home page and updates about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link} from 'gatsby'
 import Layout from '../components/layout'
-
 import Head from '../components/head'
-
 
 function AboutPage(){
   return (
     <Layout>
       <Head title="About"/>
-      <h1>About Me</h1>
-      <p>I have written end-to-end tests for an in-house sales tracking app using Cypress.</p>
-      <p>I have ran summer camps teaching HTML, CSS and JS to children ages 7 to 14.</p>
-      <p>I’m excited about exploring opportunities to grow as a software developer.</p>
-      <p> <Link to="/contact">Let's chat.</Link></p>
+      <section id="basics">
+        <h2>About Me</h2>
+        <p>I have written end-to-end tests for an in-house sales tracking app using Cypress. I have ran summer camps teaching HTML, CSS and JS to children ages 7 to 14.
+         I’m excited about exploring opportunities to grow as a software developer.</p>
+        <p>When not building sites, I spend time training to run a sub-3 hour marathon, cooking, reading, teaching and performing swing dances</p>
+        <p> <Link to="/contact">Let's chat</Link>.</p>
+      </section>
     </Layout>
   )
 }
