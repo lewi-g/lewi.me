e8d54d94e5a4168251d7863c8cc69aa5f9f36427
lewi-g <dr.lewi@gmail.com>
2020-03-17 17:28:19 -0400

    Adds headshot component and image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,13 @@ import React from "react"
 import {Link} from 'gatsby'
 import Layout from "../components/layout"
 import Head from '../components/head'
-import lewi2020 from '../../static/lewi2020.jpeg'
-import lewi2020c from '../../static/lewi2020copy.png'
-
-const imgStyle = {
-  height: "60vh",
-  width: "100%",
-  maxWidth:'750px',
-  backgroundImage: `url(${lewi2020c})`,
-  backgroundSize: 'contain',
-  backgroundRepeat: 'no-repeat',
-  marginBottom: "2rem",
-}
-
+import Headshot from '../components/headshot'
 
 function IndexPage() {
   return (
     <Layout>
       <Head title="Home"/>
-      <div role="img" aria-label="Lewi headshot" style={imgStyle}>
-      </div>
+      <Headshot/>
       <h1>Hello!</h1>
       <h2>I'm Lewi, a full-stack developer, community builder and educator living in NYC.</h2> 
       <p>Need a developer?  <Link to="/contact">Contact me</Link></p>
