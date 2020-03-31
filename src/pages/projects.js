1c4982fca6952f625d58125538d2c1f437139763
lewi-g <dr.lewi@gmail.com>
2020-03-31 09:59:22 -0400

    Adds thinxTest to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,10 +9,11 @@ function ProjectsPage(){
       <Head title ="Projects"/>
       <h2>My projects</h2>
       <ul>
-        <li> Reviewer for the <a target="_blank" href="http://graphql.guide">GraphQL Guide</a></li>
-        <li><a target="_blank" href="http://output.jsbin.com/cexajiw">Tic-Tac-Toe </a> Using 2D arrays in JavaScript to compute winner.</li>
-        <li> <a target="_blank" href="http://output.jsbin.com/mawotuk/"> Giphy Api Project</a> using raw JavaScript as an example for students.</li>
-        <li>The updated <a target="_blank" href="http://coding.space"> Coding Space</a> curriculum site</li>
+        <li><a href="https://www.github.com/lewi-g/thinxTest">thinxTest:</a> basic testing using <a target="_blank" rel="noopener noreferrer" href="https://www.cypress.io">Cypress</a> for <a rel="noopener noreferrer" href="https://www.shethinx.com">Thinx</a>.</li>
+        <li>Reviewer for the <a target="_blank" rel="noopener noreferrer" href="http://graphql.guide">GraphQL Guide</a>.</li>
+        <li><a target="_blank" rel="noopener noreferrer" href="http://output.jsbin.com/cexajiw">Tic-Tac-Toe </a> Using 2D arrays in JavaScript to compute winner.</li>
+        <li> <a target="_blank" rel="noopener noreferrer" href="http://output.jsbin.com/mawotuk/"> Giphy Api Project</a> using raw JavaScript as an example for students.</li>
+        <li>The updated <a target="_blank" rel="noopener noreferrer" href="http://coding.space"> Coding Space</a> curriculum site</li>
       </ul>
     </Layout>
   )
